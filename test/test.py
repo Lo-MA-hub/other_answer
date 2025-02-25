97d7c6052b873a7b88d0a5274e5d4073a9d01684
Peijie (Max) Ma <peijiema2004@163.com>
2025-02-25 18:52:15 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,32 +1,44 @@
+ 
+
 import cocotb
-from cocotb.triggers import Timer
-import random
+from cocotb.clock import Clock
+from cocotb.triggers import ClockCycles
 
 @cocotb.test()
-async def test_priority_encoder(dut): 
-     
-    dut.rst_n.value = 0
-    dut.ena.value = 0
+async def test_priority_encoder(dut):
+    dut._log.info("Start")
+
+    
+    clock = Clock(dut.clk, 10, units="us")
+    cocotb.start_soon(clock.start())
+
+   
+    dut._log.info("Reset")
+    dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
-    await Timer(10, units="ns")
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
-    dut.ena.value = 1
-    await Timer(10, units="ns")
- 
-    dut.ui_in.value = 0b00101010   
-    dut.uio_in.value = 0b11110001  
-    await Timer(10, units="ns")
+
+    dut._log.info("Test priority encoder behavior")
+
+    # Test case  
+    dut.ui_in.value = 0b00101010    # A[7:0]
+    dut.uio_in.value = 0b11110001   # B[7:0]
+    await ClockCycles(dut.clk, 1)
     assert dut.uo_out.value == 13, f"Test 1 failed: Expected 13, got {dut.uo_out.value}"
- 
-    dut.ui_in.value = 0b00000000
-    dut.uio_in.value = 0b00000001
-    await Timer(10, units="ns")
+
+    # Test case 
+    dut.ui_in.value = 0b00000000    # A[7:0]
+    dut.uio_in.value = 0b00000001   # B[7:0]
+    await ClockCycles(dut.clk, 1)
     assert dut.uo_out.value == 0, f"Test 2 failed: Expected 0, got {dut.uo_out.value}"
- 
-    dut.ui_in.value = 0b00000000
-    dut.uio_in.value = 0b00000000
-    await Timer(10, units="ns")
+
+    # Test case  
+    dut.ui_in.value = 0b00000000    # A[7:0]
+    dut.uio_in.value = 0b00000000   # B[7:0]
+    await ClockCycles(dut.clk, 1)
     assert dut.uo_out.value == 240, f"Test 3 failed: Expected 240, got {dut.uo_out.value}"
 
     # Additional 
@@ -35,4 +47,4 @@ async def test_priority_encoder(dut):
     await ClockCycles(dut.clk, 1)
     assert dut.uo_out.value == 15, f"Test 4 failed: Expected 15, got {dut.uo_out.value}"
 
-    print("All tests passed")
+    dut._log.info("All tests passed")
