fb3fe3bf953272c8957d10e8f354e6d92a766091
Peijie (Max) Ma <peijiema2004@163.com>
2025-02-25 18:50:40 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -9,34 +9,30 @@ async def test_priority_encoder(dut):
     dut.ena.value = 0
     dut.ui_in.value = 0
     dut.uio_in.value = 0
-    await Timer(1, units="ns")
+    await Timer(10, units="ns")
     dut.rst_n.value = 1
     dut.ena.value = 1
-    await Timer(1, units="ns")
+    await Timer(10, units="ns")
  
     dut.ui_in.value = 0b00101010   
     dut.uio_in.value = 0b11110001  
-    await Timer(1, units="ns")
-    assert dut.uo_out.value == 13, f"wrong answer: {dut.uo_out.value}, true answer is 13"
+    await Timer(10, units="ns")
+    assert dut.uo_out.value == 13, f"Test 1 failed: Expected 13, got {dut.uo_out.value}"
  
-    dut.ui_in.value = 0b10101010   
-    dut.uio_in.value = 0b11110001  
-    await Timer(1, units="ns")
-    assert dut.uo_out.value == 15, f"wrong answer: {dut.uo_out.value}, true answer is 15"
+    dut.ui_in.value = 0b00000000
+    dut.uio_in.value = 0b00000001
+    await Timer(10, units="ns")
+    assert dut.uo_out.value == 0, f"Test 2 failed: Expected 0, got {dut.uo_out.value}"
  
     dut.ui_in.value = 0b00000000
     dut.uio_in.value = 0b00000000
-    await Timer(1, units="ns")
-    assert dut.uo_out.value == 240, f"wrong answer: {dut.uo_out.value},  true nswer is 240"
- 
-    dut.ui_in.value = 0b00000000
-    dut.uio_in.value = 0b00000001
-    await Timer(1, units="ns")
-    assert dut.uo_out.value == 0, f"wrong answer: {dut.uo_out.value}, true answer is 0"
+    await Timer(10, units="ns")
+    assert dut.uo_out.value == 240, f"Test 3 failed: Expected 240, got {dut.uo_out.value}"
 
-     dut.ui_in.value = 0b00000001
-    dut.uio_in.value = 0b00000001
-    await Timer(1, units="ns")
-    assert dut.uo_out.value == 8, f"wrong answer: {dut.uo_out.value}, true answer is 8"
+    # Additional 
+    dut.ui_in.value = 0b10000000    
+    dut.uio_in.value = 0b00000000   
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 15, f"Test 4 failed: Expected 15, got {dut.uo_out.value}"
 
     print("All tests passed")
