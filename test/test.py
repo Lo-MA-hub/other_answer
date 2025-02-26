655158388f10163d8cb5ba3f874aff3493bf87a5
Peijie (Max) Ma <peijiema2004@163.com>
2025-02-25 19:29:03 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,7 +1,5 @@
-"""
-Code author: Max Ma
-Purpose: Testing if the priority encoder works 
-"""
+ 
+
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
@@ -9,8 +7,12 @@ from cocotb.triggers import ClockCycles
 @cocotb.test()
 async def test_priority_encoder(dut):
     dut._log.info("Start")
+
+    
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
+
+   
     dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
@@ -18,30 +20,31 @@ async def test_priority_encoder(dut):
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
+
     dut._log.info("Test priority encoder behavior")
+
+    # Test case  
     dut.ui_in.value = 0b00101010    # A[7:0]
     dut.uio_in.value = 0b11110001   # B[7:0]
-    await Timer(10, units="ns")
+    await ClockCycles(dut.clk, 1)
     assert dut.uo_out.value == 13, f"Test 1 failed: Expected 13, got {dut.uo_out.value}"
-    # test if the the answer is correct when it's the 1st term
+
+    # Test case 
     dut.ui_in.value = 0b00000000    # A[7:0]
     dut.uio_in.value = 0b00000001   # B[7:0]
-    await Timer(10, units="ns")
+    await ClockCycles(dut.clk, 1)
     assert dut.uo_out.value == 0, f"Test 2 failed: Expected 0, got {dut.uo_out.value}"
-    # test if the the answer is correct when 1's does not exist.
+
+    # Test case  
     dut.ui_in.value = 0b00000000    # A[7:0]
     dut.uio_in.value = 0b00000000   # B[7:0]
-    await Timer(10, units="ns")
+    await ClockCycles(dut.clk, 1)
     assert dut.uo_out.value == 240, f"Test 3 failed: Expected 240, got {dut.uo_out.value}"
 
-     # test if the the answer is correct when it's the 15th
+    # Additional 
     dut.ui_in.value = 0b10000000    
     dut.uio_in.value = 0b00000000   
-    await Timer(10, units="ns")
+    await ClockCycles(dut.clk, 1)
     assert dut.uo_out.value == 15, f"Test 4 failed: Expected 15, got {dut.uo_out.value}"
-    # test if the the answer is correct when it's the 2nd
-    dut.ui_in.value = 0b00000000    
-    dut.uio_in.value = 0b00000011   
-    await Timer(10, units="ns")
-    assert dut.uo_out.value == 1, f"Test 5 failed: Expected 1, got {dut.uo_out.value}"
-    dut._log.info("All tests passed!")
+
+    dut._log.info("All tests passed")
