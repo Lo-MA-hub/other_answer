3b189d51578d15ec1ae547b2cf08b1ef33b01f08
Peijie (Max) Ma <peijiema2004@163.com>
2025-02-25 20:03:38 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -2,9 +2,7 @@
  * Copyright (c) 2025 Your Name
  * SPDX-License-Identifier: Apache-2.0
  */
-
 `default_nettype none
-
 module tt_um_priority_encoder ( 
     input  wire [7:0] ui_in,    
     output wire [7:0] uo_out,   
@@ -15,35 +13,32 @@ module tt_um_priority_encoder (
     input  wire       clk,      
     input  wire       rst_n     
 );
-
     
     wire [15:0] combined_in;
     assign combined_in = {ui_in, uio_in};
-    // whether it's the first time to find the 1.
-    integer term;
-    integer i;
+    
     // Priority encoder logic
     reg [7:0] priority_out;
+    reg found;
+    integer i;
     
     always @(*) begin
+        priority_out = 8'hF0; // Default value when no bit is set
+        found = 1'b0;
         
-        // Default to 0, will be overridden by first 1 found
-        priority_out = 8'b11110000;
-        term = 0;
         for (i = 15; i >= 0; i = i - 1) begin
             // Check bits from MSB (15) to LSB (0)
-            if (combined_in[i] && term == 0) begin
-                priority_out = i;
-                term = 1;
+            if (combined_in[i] && !found) begin
+                priority_out = i[7:0];
+                found = 1'b1;
             end
+        end
     end
-
     
     assign uo_out = priority_out;
     assign uio_out = 8'b0;     
     assign uio_oe = 8'b0;      
-
     
-    wire _unused = &{ena, clk, rst_n, 1'b0};
-
+    // Properly handle unused signals
+    wire unused = &{ena, clk, rst_n, 1'b0};
 endmodule
